4856b53ca4d6879ef9f5e9787e6210db8aca5a52
Jonathan San Miguel <guerreroz80@outlook.es>
2022-11-01 09:38:29 -0600

    Cors listos

--- src/app.js
+++ src/app.js
@@ -8,6 +8,14 @@ const app = express();
 //MiddleWares
 app.use(morgan("dev"));
 app.use(express.json());
+// Configurar Headers y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 //Importacion de las rutas
 app.use(indexRoutes);
