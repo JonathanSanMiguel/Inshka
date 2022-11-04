8824d291373d7b7eebe5fd47ef369672eb475190
Jonathan San Miguel <guerreroz80@outlook.es>
2022-11-04 10:01:40 -0600

    Inska

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import app from './app.js';
 import { PORT } from './config.js';
 
 //Inizializacion del puerto
-app.listen(PORT);
-console.log("Servidor en el puerto ", PORT);
+app.listen(PORT, () => {
+    console.log("Servidor en el puerto ", PORT);
+});
