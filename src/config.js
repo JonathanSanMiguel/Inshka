cfda82d58dece2e003b90522b86316494db19610
Jonathan San Miguel <guerreroz80@outlook.es>
2022-11-12 09:37:27 -0600

    Inska con docker

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { config } from 'dotenv';
 
 config();
 
-export const PORT = process.env.PORT || 5000;
+export const PORT = process.env.PORT || 3000;
 export const dbHOST = process.env.DB_HOST || localhost;
 export const dbPORT = process.env.DB_PORT || 3306;
 export const dbUSER = process.env.DB_USER || root;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import { PORT } from './config.js';
 
 //Inizializacion del puerto
 app.listen(PORT, () => {
-    console.log("Servidor en el puerto ", PORT);
+    console.log("Servidor en el puerto", PORT);
 });
